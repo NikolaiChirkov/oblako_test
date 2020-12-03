270ad42ff52f9d898650c610cd799a68f2c26fe0
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-12-04 01:02:01 +0300

    OT-8: add action types

---
+++ src/Redux/Action/ActionTypes.tsx
@@ -0,0 +1,8 @@
+export const GET_LIST = "GET_LIST";
+export const POST_LIST = "POST_LIST";
+export const DELETE_LIST = "DELETE_LIST";
+export const PATCH_LIST = "PATCH_LIST";
+
+export const POST_TASK = "POST_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+export const PATCH_TASK = "PATCH_TASK";
