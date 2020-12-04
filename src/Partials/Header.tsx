84dcbf5b19bd9ebb2969c83a3fe193968a670b57
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-12-04 11:25:03 +0300

    OT-4: add header modal

--- src/Partials/Header.tsx
+++ src/Partials/Header.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Appbar } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { Appbar, Portal, Modal } from 'react-native-paper';
+import PartialStyles from './PartialStyleSheets';
+import ModalListItem from './ModalListItem';
 
 export default function Header() {
-    return (
+  const [editLists, setEditLists] = useState(false);
+
+  return (
+    <View>
       <Appbar.Header>
         <Appbar.Content title="Задачи" />
-        <Appbar.Action icon="shape-outline" onPress={() => console.log("press")} />
+        <Appbar.Action icon="shape-outline" onPress={() => setEditLists(true)} />
       </Appbar.Header>
-    );
+      <Portal>
+        <Modal 
+          visible={editLists} 
+          onDismiss={() => setEditLists(false)}
+          contentContainerStyle={PartialStyles.modalContainer}
+        >
+          <ModalListItem />
+          <TextInput style={PartialStyles.modalInput} />
+        </Modal>
+      </Portal>
+    </View>
+  );
 }
--- src/Partials/PartialStyleSheets.tsx
+++ src/Partials/PartialStyleSheets.tsx
@@ -5,6 +5,27 @@ const PartialStyles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         alignItems: "center"
+    },
+    modalContainer: {
+        flexDirection: "column",
+        alignContent: "flex-start",
+        backgroundColor: "white",
+        borderTopStartRadius: 10,
+        borderTopEndRadius: 10,
+        height: "auto"
+    },
+    modalListItem: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "white",
+        paddingHorizontal: 10,
+        borderTopEndRadius: 10,
+        borderTopStartRadius: 10,
+    },
+    modalInput: {
+        width: "90%",
+
     }
 });
 
