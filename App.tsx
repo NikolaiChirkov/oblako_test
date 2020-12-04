61f71714af16d19a449acf4c0bcd027f57429f89
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-12-04 03:21:22 +0300

    OT-1: add header template

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Colors, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
 import MainPage from './src/MainPage';
 import store from './src/Redux/Store';
 
-const theme = DefaultTheme;
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.white,
+    text: Colors.black
+  }
+};
 
 export default function App() {
   return (
--- src/Partials/Header.tsx
+++ src/Partials/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { IconButton, Colors } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 
 export default function Header() {
     return (
-      <View>
-            <Text>Задачи</Text>
-            <IconButton icon="mdi-shape-outline" color={Colors.black}></IconButton>
-      </View>
+      <Appbar.Header>
+        <Appbar.Content title="Задачи" />
+        <Appbar.Action icon="shape-outline" onPress={() => console.log("press")} />
+      </Appbar.Header>
     );
 }
